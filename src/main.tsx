318ce1cd3073a1a603cac7b6928631207668d6ca
Andrés <andres.ravelo@bioquimica.cl>
2025-06-11 08:11:36 -0400

    se agrega la lectura del comando F11 para aplicar el modo pantalla completa

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { getCurrentWindow } from '@tauri-apps/api/window';
+
+window.addEventListener('keydown', async (e) => {
+  if (e.key === 'F11') {
+    e.preventDefault(); // Evita comportamiento por defecto
+    const isFullscreen = await getCurrentWindow().isFullscreen();
+    await getCurrentWindow().setFullscreen(!isFullscreen);
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
